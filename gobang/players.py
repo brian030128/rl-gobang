95fa345e654fd273d4e93ead03488301e93c8031
Brian <brian030128@gmail.com>
2025-05-24 19:36:46 +0800

    update

--- gobang/players.py
+++ gobang/players.py
@@ -37,10 +37,20 @@ class HumanGobangPlayer():
     
 
 
-
+import argparse
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_episodes', type=int)
+    parser.add_argument('--num_iterations', type=int)
+    parser.add_argument('--keep_iters', type=int, default=20)
+    parser.add_argument('--pk_episodes', type=int, default=40)
+    parser.add_argument('--num_mcts_sims', type=int, default=25)
+    parser.add_argument('--cpuct', type=int, default=1)
+    args = parser.parse_args()
+    return args
 
 class AlphaZeroPlayer():
-    def __init__(self, game, nnet, args):
+    def __init__(self, game, nnet, args=get_args()):
         self.game = game
         self.nnet = nnet
         self.mcts = MCTS(self.game, self.nnet, args)
@@ -51,5 +61,3 @@ class AlphaZeroPlayer():
         action = np.random.choice(len(pi), p=pi)
         return action
     
-
-
