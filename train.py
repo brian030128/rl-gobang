98261abd5064b168fea5750e8505aac0aa678736
Brian <brian030128@gmail.com>
2025-05-26 18:11:42 +0800

    udpate

--- train.py
+++ train.py
@@ -247,9 +247,9 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--num_episodes', type=int , default=100)
+    parser.add_argument('--num_episodes', type=int , default=10)
     parser.add_argument('--batch_size', type=int , default=64)
-    parser.add_argument('--train_epoches', type=int , default=10)
+    parser.add_argument('--train_epoches', type=int , default=1)
     parser.add_argument('--num_iterations', type=int, default=1000)
     parser.add_argument("--wandb-run-name", type=str, default="gobang-alpha-zero")
     parser.add_argument('--keep_iters', type=int, default=20)
